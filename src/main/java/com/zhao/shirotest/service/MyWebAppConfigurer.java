e10e305449bdb9689649f02806a0b0701edc2a66
Bowen <376512291@qq.com>
2019-11-26 22:32:01 +0800

    Initial commit

---
+++ src/main/java/com/zhao/shirotest/service/MyWebAppConfigurer.java
@@ -0,0 +1,19 @@
+package com.zhao.shirotest.service;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**").addResourceLocations("file:///D:/QQ/");
+    }
+}
